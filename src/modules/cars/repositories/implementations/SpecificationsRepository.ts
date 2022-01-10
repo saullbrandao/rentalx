edb87f8b2ae406d01bcabead8815ba412f21f4ff
Saull Brandão <saull@outlook.com>
2022-01-10 11:19:39 -0300

    refactor: create a use case for creating a new specification

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -7,9 +7,20 @@ import {
 class SpecificationsRepository implements ISpecificationsRepository {
   private specifications: Specification[];
 
-  constructor() {
+  private static INSTANCE: SpecificationsRepository;
+
+  private constructor() {
     this.specifications = [];
   }
+
+  public static getInstance(): SpecificationsRepository {
+    if (!SpecificationsRepository.INSTANCE) {
+      SpecificationsRepository.INSTANCE = new SpecificationsRepository();
+    }
+
+    return SpecificationsRepository.INSTANCE;
+  }
+
   create({ name, description }: ICreateSpecificationsDTO): void {
     const specification = new Specification();
 
--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,25 +1,11 @@
 import { Router } from "express";
 
-import { SpecificationsRepository } from "../modules/cars/repositories/implementations/SpecificationsRepository";
-import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
+import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
 
 const specificationsRoutes = Router();
-const specificationsRepository = new SpecificationsRepository();
 
 specificationsRoutes.post("/", (req, res) => {
-  const { name, description } = req.body;
-  const createSpecificationService = new CreateSpecificationService(
-    specificationsRepository
-  );
-
-  createSpecificationService.execute({ name, description });
-  return res.status(201).send();
+  createSpecificationController.handle(req, res);
 });
-
-// specificationsRoutes.get("/", (req, res) => {
-//   const categories = categoriesRepository.list();
-
-//   return res.json(categories);
-// });
 
 export { specificationsRoutes };
