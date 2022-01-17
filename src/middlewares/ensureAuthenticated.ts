1898c225817597dd8aff5f649aedc47d1e010489
Saull Brandão <saull@outlook.com>
2022-01-17 05:45:33 -0300

    feat: route to add avatar to a user

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -33,6 +33,10 @@ export async function ensureAuthenticated(
       throw new AppError("User not found", 401);
     }
 
+    req.user = {
+      id: user_id,
+    };
+
     next();
   } catch {
     throw new AppError("Invalid token", 401);
