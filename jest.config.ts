c5dbf41700ec72db49395f58849482203487d388
Saull Brandão <saull@outlook.com>
2022-02-07 08:18:26 -0300

    chore: update jest config

--- jest.config.ts
+++ jest.config.ts
@@ -6,10 +6,15 @@ import { compilerOptions } from "./tsconfig.json";
 export default {
   bail: true,
   clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ["<rootDir>/src/modules/**/useCases/**/*.ts"],
+  coverageDirectory: "coverage",
   coverageProvider: "v8",
+  coverageReporters: ["lcov", "text-summary"],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
     prefix: "<rootDir>/src/",
   }),
   preset: "ts-jest",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   testMatch: ["**/*.spec.ts"],
 };
