069dacc01245351acdd19c8463a4bceaddfcb637
Saull Brandão <saull@outlook.com>
2022-02-08 11:28:07 -0300

    feat: use aws S3 to store images

--- src/modules/cars/useCases/uploadCarImage/UploadCarImagesUseCase.ts
+++ src/modules/cars/useCases/uploadCarImage/UploadCarImagesUseCase.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from "tsyringe";
 
 import { CarImage } from "@modules/cars/infra/typeorm/entities/CarImage";
 import { ICarImagesRepository } from "@modules/cars/repositories/ICarImagesRepository";
+import { IStorageProvider } from "@shared/container/providers/StorageProvider/IStorageProvider";
 
 interface IRequest {
   car_id: string;
@@ -12,22 +13,16 @@ interface IRequest {
 class UploadCarImagesUseCase {
   constructor(
     @inject("CarImagesRepository")
-    private carImagesRepository: ICarImagesRepository
+    private carImagesRepository: ICarImagesRepository,
+    @inject("StorageProvider")
+    private storageProvider: IStorageProvider
   ) {}
 
-  async execute({ car_id, images_names }: IRequest): Promise<CarImage[]> {
-    const images = await Promise.all(
-      images_names.map(async (image_name) => {
-        const image = await this.carImagesRepository.create({
-          car_id,
-          image_name,
-        });
-
-        return image;
-      })
-    );
-
-    return images;
+  async execute({ car_id, images_names }: IRequest): Promise<void> {
+    images_names.map(async (image_name) => {
+      await this.carImagesRepository.create({ car_id, image_name });
+      await this.storageProvider.save(image_name, "cars");
+    });
   }
 }
 
---
+++ src/shared/container/providers/StorageProvider/implementations/LocalStorageProvider.ts
@@ -0,0 +1,26 @@
+import fs from "fs";
+import { resolve } from "path";
+
+import upload from "@config/upload";
+import { deleteFile } from "@utils/file";
+
+import { IStorageProvider } from "../IStorageProvider";
+
+class LocalStorageProvider implements IStorageProvider {
+  async save(file: string, folder: string): Promise<string> {
+    await fs.promises.rename(
+      resolve(upload.tmpFolder, file),
+      resolve(`${upload.tmpFolder}/${folder}`, file)
+    );
+
+    return file;
+  }
+
+  async delete(file: string, folder: string): Promise<void> {
+    const filename = resolve(`${upload.tmpFolder}/${folder}`, file);
+
+    await deleteFile(filename);
+  }
+}
+
+export { LocalStorageProvider };
